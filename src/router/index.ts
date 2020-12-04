3226d69e999e95f07e786034ccbfe90bb0a1bac9
Ivan Orekhov <makito82@gmail.com>
2020-12-04 20:12:27 +0300

    add 404 page and titles for pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, { RouteConfig, Route, RawLocation } from 'vue-router'
 
 import Home from '@/views/Home.vue'
 import History from '@/views/History.vue'
+import Page404 from '@/views/404.vue'
 
 Vue.use(VueRouter)
 
@@ -10,19 +11,41 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: '/history',
     name: 'History',
-    component: History
-  }
+    component: History,
+    meta: {
+      title: 'History'
+    }
+  },
+  {
+    path: '*',
+    name: '404',
+    component: Page404,
+    meta: {
+      title: '404'
+    }
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to: Route, from: Route, next: (to?: RawLocation | false | ((vm: Vue) => any) | void) => void) => {
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title
+  }
+  next()
 })
 
 export default router
---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Page404 extends Vue {}
+</script>
