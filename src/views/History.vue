a66e37622b53b75ccdf4bdf3b135705700f820aa
Ivan Orekhov <makito82@gmail.com>
2020-12-04 09:08:33 +0300

    Create base app

---
+++ src/views/History.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="content">
+      This is History page
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class History extends Vue {}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="nav">
+      <router-link to="/history">History</router-link>
+    </div>
+    <div class="content">
+      This is Home page
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {}
+</script>
