f2ca8aa52640f9fb416140053fe1e56f590d68ed
Ivan Orekhov <makito82@gmail.com>
2020-12-04 21:23:13 +0300

    load base data on Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,35 @@
     <div class="nav">
       <router-link to="/history">History</router-link>
     </div>
-    <div class="content">
+    <div class="content" v-if="!dataIsLoading">
       This is Home page
     </div>
+    <div v-else>data is loading...</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { AxiosResponse } from 'axios'
+import { State, Action } from 'vuex-class'
+
+import { CommonEntity } from '@/api/common.types'
+import { GET_DATA } from '@/store/types'
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @State('data')
+  data!: CommonEntity[]
+
+  @State('isLoading')
+  dataIsLoading!: boolean
+
+  @Action(GET_DATA)
+  getData!: () => Promise<AxiosResponse<CommonEntity[]>>
+
+  async created() {
+    await this.getData()
+    console.log(this.data)
+  }
+}
 </script>
