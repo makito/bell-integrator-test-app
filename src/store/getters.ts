7aa743db2b1310da4803215426eb0e7a18d9117b
Ivan Orekhov <makito82@gmail.com>
2020-12-06 19:46:07 +0300

    filter by text with saved struct

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,22 +2,37 @@ import { GetterTree } from 'vuex'
 
 import { RootState, GET_SELECTED, GET_UNSELECTED } from '@/store/types'
 import CommonExtendedEntity from '@/common/common-extended-entity.interface'
-import { sortByIndex, sortByTextReverse } from '@/common/helpers'
+import { sortByTextReverse } from '@/common/helpers'
 
 export const getters: GetterTree<RootState, RootState> = {
   [GET_SELECTED](state: RootState): CommonExtendedEntity[] {
-    return state.list
-      .filter(item => item.selected)
-      .sort(sortByIndex)
+    return state.list.filter(item => item.selected)
   },
 
   [GET_UNSELECTED](state: RootState): CommonExtendedEntity[] {
-    return state.list
-      .filter(item => {
-        return !state.searchText ?
-          !item.selected :
-          !item.selected && new RegExp(state.searchText, 'i').test(item.name)
-      })
-      .sort(!state.searchText ? sortByIndex : sortByTextReverse)
+    const filtered = state.list.filter(item => {
+      return !state.searchText ?
+        !item.selected :
+        !item.selected && new RegExp(state.searchText, 'i').test(item.name)
+    })
+    
+    // если нет поиска, отдаем структуру какая была при загрузке
+    if (!state.searchText) {
+      return filtered
+    }
+
+    const sortedByText = filtered.sort(sortByTextReverse)
+    // восстанавливаем иерархию после реверсивной сортировки по name
+    return sortedByText.reduce<Array<Array<CommonExtendedEntity>>>((acc, item: CommonExtendedEntity) => {
+      if (!item.parent) {
+        const position = acc.push([item])
+        sortedByText.forEach(subitem => {
+          if (subitem.parent && subitem.parent === item.uid) {
+            acc[position - 1].push(subitem)
+          }
+        })
+      }
+      return acc
+    }, []).flat()
   }
 }
